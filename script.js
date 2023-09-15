89f441467c617ced24734e6d80c8f2b9e7a23c47
Garrett Park <garrett.park.06@icloud.com>
2023-09-15 12:45:07 +0000

    Sep 15, 2023, 8:45 AM

--- script.js
+++ script.js
@@ -83,16 +83,16 @@ function savePersonnelFileLink() {
 
   if (linkValue !== "") {
     // Construct the link in "posting" mode
-    var postingLink = personnelFileLinkInput;
+    var postingLink = personnelFileLinkInput.get();
     alert(postingLink);
 
 
-    
+    /*
     // Set the expiration date to a very large value (e.g., 10 years from now)
     const expirationDate = new Date();
     expirationDate.setFullYear(expirationDate.getFullYear() + 10); // Set expiration to 10 years from now
     document.cookie = `personnelFileLink=${postingLink}; expires=${expirationDate.toUTCString()}`;
-
+    */
     /*
     // Update the link for "Go to Personnel File" button
     const personnelFilesLink = document.getElementById("personnel-files-link"); 
