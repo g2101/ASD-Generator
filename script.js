bec374427018a4252887fcae7605feee8269cdc3
Garrett Park <72951078+g2101@users.noreply.github.com>
2023-09-15 01:06:59 +0000

    Sep 14, 2023, 9:06 PM

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 function generateBBCode() {
-    // Get the current date in UTC
-    const currentUTCDate = new Date();
-    const day = currentUTCDate.getUTCDate().toString().padStart(2, '0');
-    const month = currentUTCDate.toLocaleString('default', { month: 'short', timeZone: 'UTC' }).toUpperCase();
-    const year = currentUTCDate.getUTCFullYear();
-    const formattedDate = `${day}/${month}/${year}`;
+  // Get the current date in UTC
+  const currentUTCDate = new Date();
+  const day = currentUTCDate.getUTCDate().toString().padStart(2, "0");
+  const month = currentUTCDate
+    .toLocaleString("default", { month: "short", timeZone: "UTC" })
+    .toUpperCase();
+  const year = currentUTCDate.getUTCFullYear();
+  const formattedDate = `${day}/${month}/${year}`;
 
-    // Generate BBCode for the checkboxes
-    const checkboxes = document.querySelectorAll('.bbcode-checkbox');
-    let bbcode = `[divbox2=white][center][b]FLIGHT LOG ENTRY[/b][/center]\n[hr][/hr]\n[list=none][*][b]Date[/b]: ${formattedDate}\n[list=none]`;
+  // Generate BBCode for the checkboxes
+  const checkboxes = document.querySelectorAll(".bbcode-checkbox");
+  let bbcode = `[divbox2=white][center][b]FLIGHT LOG ENTRY[/b][/center]\n[hr][/hr]\n[list=none][*][b]Date[/b]: ${formattedDate}\n[list=none]`;
 
-    checkboxes.forEach((checkbox) => {
-        const label = checkbox.nextElementSibling;
-        const text = label.textContent.trim();
-        const isChecked = checkbox.checked;
+  checkboxes.forEach((checkbox) => {
+    const label = checkbox.nextElementSibling;
+    const text = label.textContent.trim();
+    const isChecked = checkbox.checked;
 
-        if (isChecked) {
-            bbcode += `[cb] [b]${text}[/b]\n`;
-        } else {
-            bbcode += `[cbc] [b]${text}[/b]\n`;
-        }
-    });
+    if (isChecked) {
+      bbcode += `[cb] [b]${text}[/b]\n`;
+    } else {
+      bbcode += `[cbc] [b]${text}[/b]\n`;
+    }
+  });
 
-    bbcode += `[/list][/divbox2]`;
+  bbcode += `[/list][/divbox2]`;
 
-    // Update the BBCode output textarea
-    const bbcodeOutput = document.getElementById('bbcode-output');
-    bbcodeOutput.value = bbcode;
+  // Update the BBCode output textarea
+  const bbcodeOutput = document.getElementById("bbcode-output");
+  bbcodeOutput.value = bbcode;
 }
 
 function copyToClipboard() {
-    const bbcodeOutput = document.getElementById('bbcode-output');
-    bbcodeOutput.select();
-    document.execCommand('copy');
+  const bbcodeOutput = document.getElementById("bbcode-output");
+  bbcodeOutput.select();
+  document.execCommand("copy");
 }
